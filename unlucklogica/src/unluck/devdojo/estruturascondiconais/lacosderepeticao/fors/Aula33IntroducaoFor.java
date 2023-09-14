23b8f17134eb5855da4e0f238100771888c54ae0
unluckdev <unluckdev@gmail.com>
2023-09-14 20:43:07 -0300

    Create Aula33IntroducaoFor.java

---
+++ unlucklogica/src/unluck/devdojo/estruturascondiconais/lacosderepeticao/fors/Aula33IntroducaoFor.java
@@ -0,0 +1,9 @@
+package unluck.devdojo.estruturascondiconais.lacosderepeticao.fors;
+
+public class Aula33IntroducaoFor {
+    public static void main(String[] args) {
+        for(int contador = 0; contador <= 10; contador++ ){
+            System.out.println(contador);
+        }
+    }
+}
