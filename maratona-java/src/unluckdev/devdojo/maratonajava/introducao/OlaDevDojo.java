e2879778f88f81d72117e15afae34084ce14b9a4
unluckdev <unluckdev@gmail.com>
2023-10-01 22:23:42 -0300

    okey

--- maratona-java/src/unluckdev/devdojo/maratonajava/introducao/OlaDevDojo.java
+++
@@ -1,7 +0,0 @@
-package unluckdev.devdojo.maratonajava.introducao;
-
-public class OlaDevDojo {
-    public static void main(String[] args) {
-        System.out.println("Olá Munda Java!");
-    }
-}
